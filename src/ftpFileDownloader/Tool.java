e0f25a5f449fadeda7a428618a84686176558ad4
sid-dinesh94 <f2012519@goa.bits-pilani.ac.in>
2016-03-04 22:32:07 +0530

    Remove Credentials into a file
    All credentials are now stored in a separate file so that it can be made
    more generic

--- src/ftpFileDownloader/Tool.java
+++ src/ftpFileDownloader/Tool.java
@@ -1,8 +1,12 @@
 package ftpFileDownloader;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.nio.file.*;
 
 import ftpFileDownloader.util.*;
 
@@ -10,24 +14,60 @@ public class Tool {
 	public static void error(int id){
 		if(id == -1)
 			System.out.println("Invalid use of program. Execute with flag -about for details\n"
-				+ "or with -output <directory_path> if you want to download the FTP file.\n");
+				+ "or with -output <directory_path> if you want to download the FTP file.\n"
+				+ "Please put the directory path within quotes if there are spaces within the directory path.\n");
 		if(id == -2) 
 			System.out.println("You do not have permissions to modify this directory."
-    			+ "\nPlease try afresh with another directory.");
+    			+ "\nPlease try afresh with another directory.\n");
 	}
 	public static void about(){
 		System.out.println("\u00A9 2016, Siddharth Dinesh, https://github.com/sid-dinesh94/FTPFileDownloader\n"
 				+ "List of external library dependencies\n"
 				+ "OpenCSV\n"
-				+ "Apache Commons Net\n");
+				+ "Apache Commons Net\n"
+				+ "Format to execute:"
+				+ "FTPFileDownloader.jar -output <directoryPath>"
+				+ "directory Path needs to be enclosed in quotes if it is seperated by spaces");
 		return;
 	}
-	public static void output(String relativePath){
-		String fileName = "FTP File Downloader and Processor example file"; 
+	
+	public static Boolean isDir(Path path) {
+		if (path == null || !Files.exists(path)) return false;
+		else return Files.isDirectory(path);
+	}
+	
+	public void output(String relativePath){
+		InputStream credentialsFile;
+		credentialsFile = getClass().getClassLoader().getResourceAsStream("Credentials.txt");
+		
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(credentialsFile));
+		String line;
+		
+		String [] credentials = new String[4];
+		/*
+		 * Reading from the credentials file and storing it into the credentials String array
+		 * Contents of credentials[]:
+		 * credentials[0] = hostname
+		 * credentials[1] = username
+		 * credentials[2] = password
+		 * credentials[3] = filename
+		 */
+		int i = 0;
+		try {
+			while((line = bufferedReader.readLine()) != null){
+				credentials[i] = line.split(":")[1];
+				i++;
+			}
+		} catch (IOException e1) {
+			System.out.println("Credentials file is corrupted.\n");
+			e1.printStackTrace();
+			return;
+		}
+		String fileName = credentials[3]; 
 		String downloadPath = relativePath + "/backup/" + fileName + ".zip";
 		File downloadFilePath = new File(relativePath+"/backup");
 		if (!downloadFilePath.exists()) {
-		    System.out.println("creating directory: " + downloadFilePath);
+		    System.out.println("creating temporary backup directory: " + downloadFilePath);
 		    boolean result = false;
 
 		    try{
@@ -39,12 +79,12 @@ public class Tool {
 		    	return;
 		    }        
 		    if(result) {    
-		        System.out.println("DIR created");  
+		        System.out.println("Backup Directory created");  
 		    }
 		}
 		try {
 	           FileDownloader ftpDownloader =
-	               new FileDownloader("beel.org", "f00b72a7", "WtdyYSkrvzT5hErd");
+	               new FileDownloader(credentials[0],credentials[1], credentials[2]);
 	           ftpDownloader.downloadFile(fileName+".zip", downloadPath);
 	           System.out.println("FTP File downloaded successfully");
 	           ftpDownloader.disconnect();
@@ -57,7 +97,7 @@ public class Tool {
         try {
             zipFile.unzip(downloadPath, destDirectory);
         } catch (IOException ex) {
-            // some errors occurred
+            //Make logic to try extraction again
         	System.out.println("Could not extract file\nWant to try again?");
             ex.printStackTrace();
         }
@@ -68,7 +108,7 @@ public class Tool {
 		try {
 			csv.process();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			System.out.println("Error in processing CSV file.\nWant to retry?");
 			e.printStackTrace();
 		}
@@ -87,12 +127,20 @@ public class Tool {
 			error(-1);
 			return;
 		}
-		if(args[0].equals("-about")){
+		if(args.length == 1 && args[0].equals("-about")){
 			about();
 			return;
 		}
 		else if(args.length==2 && args[0].equals("-output")){
-			output(args[1]);
+			Tool tool = new Tool();
+			File directory = new File(args[1]);
+			Boolean pathExists = directory.isDirectory();
+			if(pathExists)	tool.output(args[1]);
+			else{
+				System.out.println("The path that has been inputted is not a valid file path. Please try again.");
+			}
+		}else {
+			error(-1);
 		}
 	}
 }
--- src/ftpFileDownloader/util/ProcessCSV.java
+++ src/ftpFileDownloader/util/ProcessCSV.java
@@ -1,5 +1,6 @@
 package ftpFileDownloader.util;
 import java.io.FileReader;
+
 import java.io.IOException;
 import java.io.FileWriter;
 import com.opencsv.CSVReader;
@@ -8,36 +9,28 @@ public class ProcessCSV {
 	String filePath;
 	String writePath;
 	char seperator;
+	
 	public ProcessCSV(String path, String dest, char seperator){
 		this.filePath = path;
 		this.writePath = dest;
 		this.seperator = seperator;
 	}
+	
 	public void process() throws IOException{
 		CSVReader reader = new CSVReader(new FileReader(filePath), seperator);
 		CSVWriter writer = new CSVWriter(new FileWriter(writePath));
 	    String[] nextLine; 
-	    //int count =0;
 	    while ((nextLine = reader.readNext()) != null) {
 	       // nextLine[] is an array of values from the line
 	    	if(nextLine[0].equals("set_id")){
 	    		writer.writeNext(nextLine);
-	    		/*for(String item : nextLine){
-	    			System.out.print(item + "\t");
-	    		}
-	    		System.out.println();*/
+	    
 	    	}else if(Integer.parseInt(nextLine[0])>6500000){
 	    		writer.writeNext(nextLine);
-	    		/*for(String item : nextLine){
-	    			System.out.print(item + "\t");
-	    		}
-	    		System.out.println();*/
-    			//count ++;
-
+	    
 	    	}
 	    	
 	    }
-    	//System.out.println(count);
     	writer.close();
 	    reader.close();
 	}
